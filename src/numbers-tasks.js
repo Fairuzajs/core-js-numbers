a8356cbdb36cd025d5bbd12688a2b40cd43cdae4
fairuzajs <yaubasarova.93@mail.ru>
2024-11-09 23:56:59 +0500

    feat: add task isPrime

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -214,8 +214,15 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+
+function isPrime(n) {
+  let count = 0;
+  for (let i = 2; i <= n; i += 1) {
+    if (n % i === 0) {
+      count += 1;
+    }
+  }
+  return !(count > 1);
 }
 
 /**
